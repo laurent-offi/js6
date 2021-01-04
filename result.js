25a2836a926bed0bded71e7100160b7f874390e4
laurent-offi <laurent.courcelle.lh@gmail.com>
2021-01-04 12:05:28 +0100

    Correction exercice 6

--- result.js
+++ result.js
@@ -5,9 +5,9 @@ function showNumbers()
     	let numberOne = parseInt(document.form.num1.value);
         let numberTwo = parseFloat(document.form.num2.value);
 
-        result = (shoeSize % yearOfBirth);
+        result = (numberOne % numberTwo);
         result = result.toFixed(2);
-        
+
        		if (numberOne == "") {
         		alert("Veuillez remplir le premier champ.");
       		} else if(numberTwo == "") {
